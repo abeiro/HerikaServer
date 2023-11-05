87871836ec34ef125b4542fccb675433ca674b20
RANGROO <reecemeakings@gmail.com>
2023-11-05 12:42:07 -0500

    Basic Script Generator outline

--- ui/addons/scriptwriter/index.php
+++ ui/addons/scriptwriter/index.php
@@ -10,6 +10,8 @@
 // Path to the script file
 $scriptFile = 'scriptconf.php';
 
+$SCRIPTANIMATIONS = explode("\n", $SCRIPTANIMATIONS);
+
 error_reporting(E_ALL);
 $enginePath = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR;
 
@@ -44,7 +46,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <br>
 
-<h2>Play the Script</h2>
 <div id="playButtonContainer">
     <button type="button" class="play-button" onclick="executeScriptInBackground();">Play Script</button>
 </div>
@@ -71,7 +72,52 @@ function executeScriptInBackground() {
 <hr class="divider">
 
 <h1>ChatGPT Script Generator, Not Complete</h1>
+
 <br>
+
+<form method="post" action="save_script.php">
+        <h2>Script Rules</h2>
+        <textarea name="script" rows="10" cols="100">
+            <?php
+            include('scriptconf.php');
+            echo trim($SCRIPTRULES);
+            ?>
+        </textarea><br>
+    </form>
+
+<br>
+
+<form method="post" action="save_script.php">
+        <h2>Script Topic</h2>
+        <textarea name="script" rows="5" cols="100">
+            <?php
+            include('scriptconf.php');
+            echo trim($SCRIPTTOPIC);
+            ?>
+        </textarea><br>
+    </form>
+
+    <br>
+
+<form method="post" action="save_script.php">
+        <h2>Script Length (In Seconds)</h2>
+        <textarea name="script" rows="2" cols="6">
+            <?php
+            include('scriptconf.php');
+            echo trim($SCRIPTLENGTH);
+            ?>
+        </textarea><br>
+    </form>
+
+<h2>Script Animations</h2>
+    <textarea rows="10" cols="50" readonly>
+        <?php
+        // Assuming $SCRIPTANIMATIONS is an array
+        foreach ($SCRIPTANIMATIONS as $animation) {
+            echo $animation . "\n";
+        }
+        ?>
+    </textarea>
 
 
 
