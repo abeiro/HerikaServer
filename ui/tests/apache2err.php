9c37430a48d0ffd943e88347f63c3b48390015a3
RANGROO <reecemeakings@gmail.com>
2023-11-08 14:36:33 -0500

    Apache2 error log testing page

---
+++ ui/tests/apache2err.php
@@ -0,0 +1,29 @@
+<?php
+$distroLogPath = '/var/log/apache2/error.log'; 
+$uwampLogPath = '..\..\..\..\\bin\apache\logs\error.log'; 
+
+// Function to read and filter the error log from a given path
+function readErrorLog($errorLogPath, $logType) {
+    if (file_exists($errorLogPath) && is_readable($errorLogPath)) {
+        $errorLog = file($errorLogPath);
+        $errorLog = array_reverse($errorLog);
+        echo "<h1>Reading $logType log</h1>";
+        echo '<div style="max-height: 400px; overflow-y: scroll; background-color: black; color: white; font-size: 16px;">'; // Increase text size
+        foreach ($errorLog as $line) {
+            if (strpos($line, '[php:error]') !== false) {
+                echo htmlspecialchars($line) . "<br>";
+            }
+        }
+        echo '</div>';
+    } else {
+        echo "Error log file not found or not readable at: " . $errorLogPath;
+    }
+}
+
+// Try to read the Distro log, and if it's not available, read the UWAMP log
+if (file_exists($distroLogPath) && is_readable($distroLogPath)) {
+    readErrorLog($distroLogPath, "DwemerDistro");
+} elseif (file_exists($uwampLogPath) && is_readable($uwampLogPath)) {
+    readErrorLog($uwampLogPath, "UWAMP");
+}
+?>
