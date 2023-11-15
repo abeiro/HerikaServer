b9792f51a660a4a05bf73f656fc8e5e0f1dc3d4f
RANGROO <reecemeakings@gmail.com>
2023-11-15 15:03:39 -0500

    Added export DB function

---
+++ ui/export_db.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+</head>
+<body>
+
+<?php
+
+// Specify the path to the original SQLite database file
+$originalDbFile = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "mysqlitedb.db";
+
+// Download the existing SQLite database file
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="mysqlitedb.db"');
+readfile($originalDbFile);
+exit;
+
+?>
+</body>
+</html>
