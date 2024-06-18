65ce3510b7196c481bd24a683da024f978aca0e2
Augusto Beiro <augusto.beiro@gmail.com>
2024-06-18 06:55:09 +0200

    * Missing require in tests

--- ui/tests.php
+++ ui/tests.php
@@ -38,6 +38,7 @@ $enginePath = dirname((__FILE__)) . DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR
 require_once($enginePath . "conf".DIRECTORY_SEPARATOR."conf.php");
 require_once($enginePath . "lib" .DIRECTORY_SEPARATOR."model_dynmodel.php");
 require_once($enginePath . "lib" .DIRECTORY_SEPARATOR."{$GLOBALS["DBDRIVER"]}.class.php");
+require_once($enginePath . "lib" .DIRECTORY_SEPARATOR."data_functions.php");
 
 $FEATURES["MEMORY_EMBEDDING"]["ENABLED"]=false;
 
