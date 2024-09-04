7823a3921b59291c167ac5941a648117fa888249
RANGROO <reecemeakings@gmail.com>
2024-09-04 19:33:31 -0400

    manager fix

--- ui/tmpl/navbar.php
+++ ui/tmpl/navbar.php
@@ -119,7 +119,7 @@
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Configuration</a>
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="conf_wizard.php">Configuration Wizard</a></li>
-                    <li><a class="dropdown-item" href="xtts_manager.php" title="Manually manage XTTS FastAPI voices" target="_blank">XTTS FastAPI Voice Management</a></li>
+                    <li><a class="dropdown-item" href="xtts_clone.php" title="Manually manage XTTS FastAPI voices" target="_blank">XTTS FastAPI Voice Management</a></li>
                     <li><a class="dropdown-item" href="conf_editor.php" target="_blank">Manual Configuration (Advanced!)</a></li>
                     <li><a class="dropdown-item" href='https://docs.google.com/spreadsheets/d/1cLoJRT1AsjoICg8E4PzXylsWUSYzqlKvj32F6Q5clpg/edit?gid=0#gid=0' target="_blank">AI/LLM Supported Models List</a></li>
                 </ul>
