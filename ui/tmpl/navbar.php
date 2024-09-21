9d9878efa8ef35dee73b74dc0de47ddaad9ae0cd
RANGROO <reecemeakings@gmail.com>
2024-09-21 13:22:12 -0400

    Added upload npc page

--- ui/tmpl/navbar.php
+++ ui/tmpl/navbar.php
@@ -128,9 +128,11 @@
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Configuration</a>
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="conf_wizard.php">Configuration Wizard</a></li>
+                    <li><a class="dropdown-item" href="npc_upload.php" title="Upload NPC Biographies" target="_blank">Upload NPC Biographies</a></li>
                     <li><a class="dropdown-item" href="xtts_clone.php" title="Manually manage XTTS FastAPI voices" target="_blank">XTTS FastAPI Voice Management</a></li>
                     <li><a class="dropdown-item" href="http://localhost:59125" title="Find Mimic3 voices" target="_blank">Mimic3 Voice Menu</a></li>
                     <li><a class="dropdown-item" href='https://docs.google.com/spreadsheets/d/1cLoJRT1AsjoICg8E4PzXylsWUSYzqlKvj32F6Q5clpg/edit?gid=0#gid=0' target="_blank">AI/LLM Supported Models List</a></li>
+
                 </ul>
             </li>
 
