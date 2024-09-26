022dc4a0a851dad0af05b62693cc823ed10a0b9a
RANGROO <reecemeakings@gmail.com>
2024-09-26 17:01:53 -0400

    styling changes

--- ui/conf_wizard.php
+++ ui/conf_wizard.php
@@ -311,7 +311,7 @@ echo str_repeat("</fieldset>", $lvl2);
 echo '</form>';
 
 echo "<div style='position:fixed;top:0px;right:25px;background-color:black;font-size:1em;border:1px solid grey;margin:85px 5px;padding:5px;'>
-<span><strong>Quick Access for <span style='color:green'>{$GLOBALS["CURRENT_PROFILE_CHAR"]}</span><br/><span style='font-size:7px'>".
+<span><strong>Quick Access for <span style='color:yellow'>{$GLOBALS["CURRENT_PROFILE_CHAR"]}</span><br><span style='font-size:11px'>You must click save before using 'Copy to All Profiles'</span><br/><span style='font-size:7px'>".
     basename($_SESSION["PROFILE"])
 ."</span></strong></span><ul>";
 //echo "<li><a href='#top'>Top</a></li>";
