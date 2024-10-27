daf8c89929559d78d546cb87bc4340097b1b2e3f
Ostim Tools <dwss1988@protonmail.com>
2024-10-27 16:33:53 -0400

    added ability to inject custom logic for dialogue prompt before it's used for AIFF prompts

--- prompts/prompts.php
+++ prompts/prompts.php
@@ -1,52 +1,6 @@
 <?php
 
-
-// New structure
-// $PROMPTS["event"]["cue"] => array containing cues. This is the last text sent to LLM, should be an guided instruction
-// $PROMPTS["event"]["player_request"] => array containing requirements. This is what is the player requesting for (a question, a comment...)
-// $PROMPTS["event"]["extra"] =>  enable/disable, force mod, change token limit or define a transformer (non IA related) function.
-// Full Prompt then is $PROMPT_HEAD + $HERIKA_PERS + $COMMAND_PROMPT + CONTEXT + requirement + cue
-
-// Common patterns to use in most functions
-$MAXIMUM_WORDS=($GLOBALS["MAX_WORDS_LIMIT"]>0)?"(Maximum {$GLOBALS["MAX_WORDS_LIMIT"]} words)":"";
-
-$TEMPLATE_DIALOG="write {$GLOBALS["HERIKA_NAME"]}'s next dialogue line using this format \"{$GLOBALS["HERIKA_NAME"]}: ";
-
-if (@is_array($GLOBALS["TTS"]["AZURE"]["validMoods"]) &&  sizeof($GLOBALS["TTS"]["AZURE"]["validMoods"])>0) 
-    if ($GLOBALS["TTSFUNCTION"]=="azure")
-        $TEMPLATE_DIALOG.="(optional way of speaking from this list [" . implode(",", $GLOBALS["TTS"]["AZURE"]["validMoods"]) . "])";
-
-$TEMPLATE_DIALOG.=" \"";
-
-
-
-if (isset($GLOBALS["FEATURES"]["MEMORY_EMBEDDING"]["ENABLED"]) && $GLOBALS["FEATURES"]["MEMORY_EMBEDDING"]["ENABLED"]) {
-    $GLOBALS["MEMORY_STATEMENT"]=".USE #MEMORY.";
-} else
-    $GLOBALS["MEMORY_STATEMENT"]="";
-
-
-if ($GLOBALS["FUNCTIONS_ARE_ENABLED"]) {
-    $TEMPLATE_ACTION="call a function to control {$GLOBALS["HERIKA_NAME"]} or";
-    $TEMPLATE_ACTION=".USE TOOL CALLING.";    // WIP
-} else {
-    $TEMPLATE_ACTION="";
-}
-
-if (DMgetCurrentModel()=="openaijson") {
-    $TEMPLATE_DIALOG="write {$GLOBALS["HERIKA_NAME"]}'s next dialogue lines. Avoid narrations.";
-    $TEMPLATE_ACTION="";
-}
-
-if (DMgetCurrentModel()=="koboldcppjson") {
-    $TEMPLATE_DIALOG="write {$GLOBALS["HERIKA_NAME"]}'s next dialogue lines. Avoid narrations.";
-    $TEMPLATE_ACTION="";
-}
-
-if (DMgetCurrentModel()=="openrouterjson") {
-    $TEMPLATE_DIALOG="write {$GLOBALS["HERIKA_NAME"]}'s next dialogue lines. Avoid narrations.";
-    $TEMPLATE_ACTION="";
-}
+require_once("dialogue_prompt.php");
 
 $PROMPTS=array(
     "location"=>[
@@ -236,9 +190,6 @@ $PROMPTS=array(
         "player_request"=>["The Narrator:  {$gameRequest[3]}"],
     ],
 );
-
-
-
 
 if (isset($GLOBALS["CORE_LANG"]))
 	if (file_exists(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."lang".DIRECTORY_SEPARATOR.$GLOBALS["CORE_LANG"].DIRECTORY_SEPARATOR."prompts.php")) 
