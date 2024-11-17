f1fdf8f54e7d479add680a5d64ce5cbe676c4f90
daikichi <dekanaberserker@gmail.com>
2024-11-17 22:46:18 +0900

    add button to copy connector to all profiles

---
+++ ui/cmd/action_copy_connector_to_all.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+$enginePath = dirname((__FILE__)) . DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."../";
+require_once($enginePath . "conf".DIRECTORY_SEPARATOR."conf.php");
+require_once($enginePath . "lib" .DIRECTORY_SEPARATOR."model_dynmodel.php");
+require_once($enginePath . "lib" .DIRECTORY_SEPARATOR."{$GLOBALS["DBDRIVER"]}.class.php");
+require_once($enginePath . "lib" .DIRECTORY_SEPARATOR."data_functions.php");
+
+if (isset($_SESSION["PROFILE"])) {
+    require_once($_SESSION["PROFILE"]);
+}
+
+$GLOBALS["active_profile"]=md5($GLOBALS["HERIKA_NAME"]);
+$newModel=DMcopyModel();
+
+if (isset($_SERVER['HTTP_REFERER'])) {
+    // Redirect to the referring page
+    header("Location: " . $_SERVER['HTTP_REFERER']);
+    exit; // Make sure to call exit after header redirection
+} else {
+    // Fallback if no referring page is set
+    header("Location: index.php");
+    exit; // Make sure to call exit after header redirection
+}
+
+?>
