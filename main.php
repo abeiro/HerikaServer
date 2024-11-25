1279d12aa61ae5f553eae8bf3ed2f10be0edf295
daikichi <dekanaberserker@gmail.com>
2024-11-25 12:01:15 +0900

    and one spelling error that bothers me <3

--- main.php
+++ main.php
@@ -391,6 +391,7 @@ require(__DIR__.DIRECTORY_SEPARATOR."prompt.includes.php");
 // Take care of override request if needed..
 require(__DIR__.DIRECTORY_SEPARATOR."processor".DIRECTORY_SEPARATOR."request.php");
 
+$gameRequest[0] = strtolower($gameRequest[0]); // one more time in case it was changed by an extension
 
 /*
  Safe stop
