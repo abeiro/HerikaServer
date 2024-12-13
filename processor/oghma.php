3b69f1f6e22b1111caeff8113df361aaad2dfd10
RANGROO <reecemeakings@gmail.com>
2024-12-13 10:52:24 -0500

    topic res fix

--- processor/oghma.php
+++ processor/oghma.php
@@ -134,7 +134,7 @@ if ($GLOBALS["MINIME_T5"]) {
                         'rank_any'=> -1,
                         'rank_all'=>-1,
                         'memory'=>"$currentInputTopic / $currentOghmaTopic / $locationCtxQuery / $contextKeywordsQuery => ",
-                        'time'=>$topic_req["elapsed_time"]
+                        'time'=>$topic_res["elapsed_time"]
                     )
                 );
             }
