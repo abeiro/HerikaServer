e2f116da52265ccea8bcf0fa729aed6be1cb3831
RANGROO <reecemeakings@gmail.com>
2024-12-14 17:46:13 -0500

    revert quickstart menu

--- ui/quickstart.php
+++ ui/quickstart.php
@@ -4,9 +4,6 @@ header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1
 header("Pragma: no-cache"); // HTTP 1.0
 header("Expires: 0"); // Proxies
 
-require_once(__DIR__."/../debug/db_updates.php");
-require_once(__DIR__."/../debug/npc_removal.php");
-
 ?>
 <!DOCTYPE html>
 <html>
