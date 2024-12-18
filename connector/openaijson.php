cefeea95074cb20b98507e1d010cd6803f021eaa
daikichi <dekanaberserker@gmail.com>
2024-12-18 09:54:08 +0900

    fix spacing

--- connector/openaijson.php
+++ connector/openaijson.php
@@ -68,69 +68,69 @@ class connector
         } else
             $speechReinforcement="";
 
-            if (isset($GLOBALS["FEATURES"]["MISC"]["JSON_DIALOGUE_FORMAT_REORDER"])&&($GLOBALS["FEATURES"]["MISC"]["JSON_DIALOGUE_FORMAT_REORDER"])) {
-                
-                if (isset($GLOBALS["LANG_LLM_XTTS"])&&($GLOBALS["LANG_LLM_XTTS"])) {
-                    $contextData[]= [
-                        'role' => 'user', 
-                        'content' => "{$prefix}. $speechReinforcement Use this JSON object to give your answer : ".json_encode([
-                            "character"=>$GLOBALS["HERIKA_NAME"],
-                            "listener"=>"specify who {$GLOBALS["HERIKA_NAME"]} is talking to",
-                            "message"=>'lines of dialogue',
-                            "mood"=>implode("|",$moods),
-                            "action"=>'a valid action, (refer to available actions list) or None',
-                            "target"=>"action's target",
-                            "lang"=>"en|es",
-                            
-                            
-                        ])
-                    ];
-                } else {
-                    $contextData[]= [
-                        'role' => 'user', 
-                        'content' => "{$prefix}. $speechReinforcement Use this JSON object to give your answer : ".json_encode([
-                            "character"=>$GLOBALS["HERIKA_NAME"],
-                            "listener"=>"specify who {$GLOBALS["HERIKA_NAME"]} is talking to",
-                            "message"=>'lines of dialogue',
-                            "mood"=>implode("|",$moods),
-                            "action"=>'a valid action, (refer to available actions list) or None',
-                            "target"=>"action's target",
-                            
-                            
-                        ])
-                    ];
-                }
-    
+        if (isset($GLOBALS["FEATURES"]["MISC"]["JSON_DIALOGUE_FORMAT_REORDER"])&&($GLOBALS["FEATURES"]["MISC"]["JSON_DIALOGUE_FORMAT_REORDER"])) {
+            
+            if (isset($GLOBALS["LANG_LLM_XTTS"])&&($GLOBALS["LANG_LLM_XTTS"])) {
+                $contextData[]= [
+                    'role' => 'user', 
+                    'content' => "{$prefix}. $speechReinforcement Use this JSON object to give your answer : ".json_encode([
+                        "character"=>$GLOBALS["HERIKA_NAME"],
+                        "listener"=>"specify who {$GLOBALS["HERIKA_NAME"]} is talking to",
+                        "message"=>'lines of dialogue',
+                        "mood"=>implode("|",$moods),
+                        "action"=>'a valid action, (refer to available actions list) or None',
+                        "target"=>"action's target",
+                        "lang"=>"en|es",
+                        
+                        
+                    ])
+                ];
             } else {
-                if (isset($GLOBALS["LANG_LLM_XTTS"])&&($GLOBALS["LANG_LLM_XTTS"])) {
-                    $contextData[]= [
-                        'role' => 'user', 
-                        'content' => "{$prefix}. $speechReinforcement Use this JSON object to give your answer : ".json_encode([
-                            "character"=>$GLOBALS["HERIKA_NAME"],
-                            "listener"=>"specify who {$GLOBALS["HERIKA_NAME"]} is talking to",
-                            "mood"=>implode("|",$moods),
-                            "action"=>'a valid action, (refer to available actions list) or None',
-                            "target"=>"action's target",
-                            "lang"=>"en|es",
-                            "message"=>'lines of dialogue',
-                            
-                        ])
-                    ];
-                } else {
-                    $contextData[]= [
-                        'role' => 'user', 
-                        'content' => "{$prefix}. $speechReinforcement Use this JSON object to give your answer : ".json_encode([
-                            "character"=>$GLOBALS["HERIKA_NAME"],
-                            "listener"=>"specify who {$GLOBALS["HERIKA_NAME"]} is talking to",
-                            "mood"=>implode("|",$moods),
-                            "action"=>'a valid action, (refer to available actions list) or None',
-                            "target"=>"action's target",
-                            "message"=>'lines of dialogue',
-                            
-                        ])
-                    ];
-                }
+                $contextData[]= [
+                    'role' => 'user', 
+                    'content' => "{$prefix}. $speechReinforcement Use this JSON object to give your answer : ".json_encode([
+                        "character"=>$GLOBALS["HERIKA_NAME"],
+                        "listener"=>"specify who {$GLOBALS["HERIKA_NAME"]} is talking to",
+                        "message"=>'lines of dialogue',
+                        "mood"=>implode("|",$moods),
+                        "action"=>'a valid action, (refer to available actions list) or None',
+                        "target"=>"action's target",
+                        
+                        
+                    ])
+                ];
             }
+
+        } else {
+            if (isset($GLOBALS["LANG_LLM_XTTS"])&&($GLOBALS["LANG_LLM_XTTS"])) {
+                $contextData[]= [
+                    'role' => 'user', 
+                    'content' => "{$prefix}. $speechReinforcement Use this JSON object to give your answer : ".json_encode([
+                        "character"=>$GLOBALS["HERIKA_NAME"],
+                        "listener"=>"specify who {$GLOBALS["HERIKA_NAME"]} is talking to",
+                        "mood"=>implode("|",$moods),
+                        "action"=>'a valid action, (refer to available actions list) or None',
+                        "target"=>"action's target",
+                        "lang"=>"en|es",
+                        "message"=>'lines of dialogue',
+                        
+                    ])
+                ];
+            } else {
+                $contextData[]= [
+                    'role' => 'user', 
+                    'content' => "{$prefix}. $speechReinforcement Use this JSON object to give your answer : ".json_encode([
+                        "character"=>$GLOBALS["HERIKA_NAME"],
+                        "listener"=>"specify who {$GLOBALS["HERIKA_NAME"]} is talking to",
+                        "mood"=>implode("|",$moods),
+                        "action"=>'a valid action, (refer to available actions list) or None',
+                        "target"=>"action's target",
+                        "message"=>'lines of dialogue',
+                        
+                    ])
+                ];
+            }
+        }
         
 
 
