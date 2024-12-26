421e76e116b40a7be1604418181c16d0636a7f38
daikichi <dekanaberserker@gmail.com>
2024-12-26 11:40:52 +0900

    in case parenthesis closes early

--- lib/data_functions.php
+++ lib/data_functions.php
@@ -520,7 +520,7 @@ function DataLastDataExpandedFor($actor, $lastNelements = -10,$sqlfilter="")
         
         $string = $row["location"];
         preg_match('/Context\s*(new\s*)?location:\s*([^$,]+)/', $string, $locationMatch);
-        preg_match('/Hold:\s*([^$,]+)/', $string, $holdMatch);
+        preg_match('/Hold:\s*([^$,\)]+)/', $string, $holdMatch);
         
         if (!isset($holdMatch[1])) {
             //error_log(print_r($string,true));
