d2d7a79f47e4f81cdbb1c357918224d2cd78c3b0
Augusto Beiro <augusto.beiro@gmail.com>
2025-01-13 10:27:01 +0100

    * Used Daikichi solution as is better

--- data/npc_templates_trl_v1.sql
+++ data/npc_templates_trl_v1.sql
@@ -16,6 +16,9 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE IF EXISTS ONLY public.npc_templates_trl DROP CONSTRAINT npc_templates_trl_pk;
+DROP TABLE IF EXISTS public.npc_templates_trl;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
