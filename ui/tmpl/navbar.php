facc307c8d157d6ece1daabdbea4a2317921b850
RANGROO <reecemeakings@gmail.com>
2025-01-20 08:24:46 -0500

    fixed notes

--- ui/tmpl/navbar.php
+++ ui/tmpl/navbar.php
@@ -291,7 +291,7 @@
                     <li><a class="dropdown-item" href='index.php?plugins_show=true'>Plugin Manager</a></li>
                     <li><hr class="dropdown-divider"></li>
                     <li><h6 class="dropdown-header">Debugging</h6></li>
-                    <li><a class="dropdown-item" href="index.php?table=responselog" title="">Responses</a></li>
+                    <li><a class="dropdown-item" href="index.php?table=responselog" title="">Response Queue</a></li>
                     <li><a class="dropdown-item" href="index.php?table=audit_request" title="">Request Logs</a></li>
                     <div style="
                     display: flex; 
