7cd69bb29bc9fcf12b2be48e0f3e0df44033952b
id1001-gthb <id1001@gmail.com>
2025-02-19 12:36:56 +0200

    SQL functions to convert gamets to string date time (2)
    file db_update_date_time_functions.sql
    SQL functions to convert game timestamp (gamets) to string date time, various formats.
    file db_updates.php - database update

--- debug/db_updates.php
+++ debug/db_updates.php
@@ -542,5 +542,178 @@ if (checkVersion("npc_templates_custom")<20250211001) {
     error_log("Applied patch 20250211001");
 }
 
+//----------------------------------------------------
+// SQL convert gamets timestamp to date time formatted
+//  sql_gamets_convert_functions 20250218001
+//----------------------------------------------------
+
+if (checkVersion("sql_gamets_convert_functions")<20250218001) {
+    error_log(" try patch: sql_gamets_convert_functions 20250218001 - dbg -");
+
+    $db->execQuery("DROP VIEW IF EXISTS public.speech_view;");
+    $db->execQuery("DROP VIEW IF EXISTS public.eventlog_view;");
+
+    $db->execQuery("DROP FUNCTION IF EXISTS public.convert_gamets2days(gamets bigint) CASCADE;");
+    $db->execQuery("DROP FUNCTION IF EXISTS public.convert_gamets2gregorian_date(gamets bigint) CASCADE;");
+    $db->execQuery("DROP FUNCTION IF EXISTS public.convert_gamets2skyrim_long_date(gamets bigint) CASCADE;");
+    $db->execQuery("DROP FUNCTION IF EXISTS public.convert_gamets2skyrim_long_date2(gamets bigint) CASCADE;");
+    $db->execQuery("DROP FUNCTION IF EXISTS public.convert_gamets2skyrim_date(gamets bigint) CASCADE;");
+    $db->execQuery("DROP FUNCTION IF EXISTS public.convert_gamets2hours(gamets bigint) CASCADE;");
+
+    $db->execQuery("
+        CREATE OR REPLACE FUNCTION public.convert_gamets2days(gamets bigint) RETURNS bigint
+            LANGUAGE plpgsql
+            AS $$
+            BEGIN
+                RETURN floor(gamets * 0.0000001);
+            END;
+        $$;  ");
+
+    $db->execQuery("
+        CREATE OR REPLACE FUNCTION public.convert_gamets2gregorian_date(gamets bigint) RETURNS text
+            LANGUAGE plpgsql
+            AS $$
+            BEGIN
+                RETURN to_char(to_timestamp('1577.08.17 00:00:00','YYYY.MM.DD HH24:MI:SS') + (gamets * 0.0000024) * INTERVAL '1 hour', 'YYYY-MM-DD HH24:MI:SS');
+            END;
+        $$;  ");
+
+    $db->execQuery("
+        CREATE OR REPLACE FUNCTION public.convert_gamets2hours(gamets bigint) RETURNS bigint
+            LANGUAGE plpgsql
+            AS $$
+            BEGIN
+                RETURN floor(gamets * 0.0000024);
+            END;
+        $$; ");
+
+    $db->execQuery("
+        CREATE OR REPLACE FUNCTION public.convert_gamets2skyrim_date(gamets bigint) RETURNS text
+            LANGUAGE plpgsql
+            AS $$
+            BEGIN
+                RETURN to_char(to_timestamp('0201.08.17 00:00:00','YYYY.MM.DD HH24:MI:SS') + (gamets * 0.0000024) * INTERVAL '1 hour', 'YYYY-MM-DD HH24:MI:SS');
+            END;
+        $$; ");
+
+    $db->execQuery("
+        CREATE OR REPLACE FUNCTION public.convert_gamets2skyrim_long_date(gamets bigint) RETURNS text
+            LANGUAGE plpgsql
+            AS $$
+            DECLARE 
+                s_date1 text; 
+                s_date2 text; 
+                s_date3 text; 
+                s_month text;
+                s_dayweek text;
+                s_dayname text;
+                s_longm text;
+                f_hours float;
+                ts_base timestamp;
+                ts2 timestamp;
+                s_res text;
+            BEGIN
+                f_hours := (gamets * 0.0000024);
+                ts_base := to_timestamp('0201.08.17 00:00:00','YYYY.MM.DD HH24:MI:SS');
+                ts2 := ts_base  + f_hours * INTERVAL '1 hour';
+                s_month := to_char(ts2, 'MM');
+                s_dayweek := to_char(ts2, 'D'); -- D	day of the week, 
+                CASE s_dayweek
+                    WHEN '2' THEN s_dayname := 'Sundas'; -- sunday
+                    WHEN '3' THEN s_dayname := 'Morndas';
+                    WHEN '4' THEN s_dayname := 'Tirdas';
+                    WHEN '5' THEN s_dayname := 'Middas';
+                    WHEN '6' THEN s_dayname := 'Turdas';
+                    WHEN '7' THEN s_dayname := 'Fredas';
+                    WHEN '1' THEN s_dayname := 'Loredas'; -- saturday
+                    ELSE s_dayname := 'unknown day';
+                END CASE;
+                CASE s_month
+                    WHEN '01' THEN s_longm := 'Morning Star';
+                    WHEN '02' THEN s_longm := 'Sun''s Dawn';
+                    WHEN '03' THEN s_longm := 'First Seed';
+                    WHEN '04' THEN s_longm := 'Rain''s Hand';
+                    WHEN '05' THEN s_longm := 'Second Seed';
+                    WHEN '06' THEN s_longm := 'Mid Year';
+                    WHEN '07' THEN s_longm := 'Sun''s Height';
+                    WHEN '08' THEN s_longm := 'Last Seed';
+                    WHEN '09' THEN s_longm := 'Hearthfire';
+                    WHEN '10' THEN s_longm := 'Frost Fall';
+                    WHEN '11' THEN s_longm := 'Sun''s Dusk';
+                    WHEN '12' THEN s_longm := 'Evening Star';
+                    ELSE s_longm := 'unknown month';
+                END CASE;
+                s_date1 := to_char(ts2, 'HH12:MI AM');
+                s_date2 := to_char(ts2, 'FMDD');
+                s_date3 := to_char(ts2, ', 4E FMYYYY');
+                s_res := s_dayname || ', ' || s_date1 || ', ' || s_date2 ||  'th of ' || s_longm || s_date3;
+                RETURN s_res;
+            END;
+        $$; ");
+
+    $db->execQuery("
+        CREATE OR REPLACE FUNCTION public.convert_gamets2skyrim_long_date2(gamets bigint) RETURNS text
+            LANGUAGE plpgsql
+            AS $$
+            DECLARE 
+                s_date1 text; 
+                s_date2 text; 
+                s_month text;
+                s_longm text;
+                f_hours float;
+                ts_base timestamp;
+                ts2 timestamp;
+                s_res text;
+            BEGIN
+                f_hours := (gamets * 0.0000024);
+                ts_base := to_timestamp('0201.08.17 00:00:00','YYYY.MM.DD HH24:MI:SS');
+                ts2 := ts_base  + f_hours * INTERVAL '1 hour';
+                s_month := to_char(ts2, 'MM');
+                CASE s_month
+                    WHEN '01' THEN s_longm := 'Morning Star';
+                    WHEN '02' THEN s_longm := 'Sun''s Dawn';
+                    WHEN '03' THEN s_longm := 'First Seed';
+                    WHEN '04' THEN s_longm := 'Rain''s Hand';
+                    WHEN '05' THEN s_longm := 'Second Seed';
+                    WHEN '06' THEN s_longm := 'Mid Year';
+                    WHEN '07' THEN s_longm := 'Sun''s Height';
+                    WHEN '08' THEN s_longm := 'Last Seed';
+                    WHEN '09' THEN s_longm := 'Hearthfire';
+                    WHEN '10' THEN s_longm := 'Frost Fall';
+                    WHEN '11' THEN s_longm := 'Sun''s Dusk';
+                    WHEN '12' THEN s_longm := 'Evening Star';
+                    ELSE s_longm := 'unknown';
+                END CASE;
+                s_date1 := to_char(ts2, 'DD');
+                s_date2 := to_char(ts2, ' 4E FMYYYY, HH24:MI');
+                s_res := s_date1 || 'th of ' || s_longm || s_date2;
+                RETURN s_res;
+            END;
+        $$; ");
+
+    $db->execQuery("
+        CREATE OR REPLACE VIEW public.eventlog_view AS
+          SELECT e.*,
+            public.convert_gamets2skyrim_date(e.gamets) AS sk_date,
+            public.convert_gamets2skyrim_long_date(e.gamets) AS sk_long_date,
+            public.convert_gamets2days(e.gamets) AS sk_days,
+            public.convert_gamets2gregorian_date(e.gamets) AS gregorian_date
+          FROM public.eventlog e; ");
+
+    $db->execQuery("
+        CREATE OR REPLACE VIEW public.speech_view AS
+          SELECT s.*,
+            public.convert_gamets2skyrim_date(s.gamets) AS sk_date,
+            public.convert_gamets2skyrim_long_date(s.gamets) AS sk_long_date,
+            public.convert_gamets2days(s.gamets) AS sk_days,
+            public.convert_gamets2gregorian_date(s.gamets) AS gregorian_date
+          FROM public.speech s; ");
+    
+    updateVersion("sql_gamets_convert_functions",20250218001);
+    updateVersion("sql_gamets_convert_functions",20250218001);
+    error_log("Applied patch: sql_gamets_convert_functions 20250218001 - dbg -");
+}
+
+//----------------------------------------------------
 
 ?>
