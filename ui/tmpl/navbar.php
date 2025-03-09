3432fbaf12007a46ff366795e2d06f53ba4d1c4e
RANGROO <reecemeakings@gmail.com>
2025-03-09 17:02:29 -0400

    fixed double configuration

--- ui/tmpl/navbar.php
+++ ui/tmpl/navbar.php
@@ -346,69 +346,10 @@ echo '<link rel="stylesheet" href="' . $webRoot . '/ui/css/navbar.css">';
                     </li>
                 </ul>
             </li>
-
-            <li class="nav-item dropdown mx-2">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Configuration</a>
-                <ul class="dropdown-menu">
-
-                    
-                    <li><h6 class="dropdown-header">Configuration Tools</h6></li>
-                    <li>
-                    <a class="dropdown-item" href="conf_wizard.php">Configuration Wizard</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="npc_upload.php" title="Edit NPC biographies entries" target="_blank">
-                        NPC Biography Management
-                    </a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="oghma_upload.php" title="Edit Oghma Infinium entries" target="_blank">
-                        Oghma Infinium Management
-                    </a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="customprompteditor.php" target="_blank">
-                    Custom Prompt Editor
-                    </a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="quickstart.php" target="_blank">
-                        Quickstart Menu
-                    </a>
-                    </li>
-
-
-                    <li><hr class="dropdown-divider"></li>
-                    <li><h6 class="dropdown-header">TTS Voice Management</h6></li>
-                    <li>
-                    <a class="dropdown-item" href="xtts_clone.php" title="Manually manage XTTS FastAPI voices" target="_blank" rel="noopener noreferrer">
-                        CHIM XTTS Management
-                    </a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="http://localhost:59125" title="Find Mimic3 voices" target="_blank">
-                        Mimic3 Management
-                    </a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="<?= htmlspecialchars($GLOBALS["TTS"]["ZONOS_GRADIO"]["endpoint"]) ?>" title="Test Zonos Settings" target="_blank">
-                        Zonos Gradio Management
-                    </a>
-                    </li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><h6 class="dropdown-header">Web Extensions</h6></li>
-                    <li>
-                    <a class="dropdown-item" href="addons/pmstt" target="_blank">Chrome Free Speech-to-Text</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="addons/websocket" target="_blank">Websocket Configuration (WIP)</a>
-                    </li>
-                </ul>
-            </li>
             <li class="nav-item dropdown mx-2">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Guides</a>
                 <ul class="dropdown-menu">
-                    <li><h6 class="dropdown-header">PLEASE READ!</h6></li>
+                    <li><h6 class="dropdown-header">GUIDES</h6></li>
                     <li><a class="dropdown-item" href='<?php echo $webRoot; ?>/ui/index.php?notes=true'>CHIM 101 Quick Guide</a></li>
                     <li><a class="dropdown-item" href='https://docs.google.com/document/d/12KBar_VTn0xuf2pYw9MYQd7CKktx4JNr_2hiv4kOx3Q/edit?usp=sharing' target="_blank">CHIM Manual</a></li>
                     <li><a class="dropdown-item" href="https://docs.google.com/spreadsheets/d/1cLoJRT1AsjoICg8E4PzXylsWUSYzqlKvj32F6Q5clpg/edit?gid=0#gid=0" target="_blank">AI/LLM Supported Models List</a></li>
