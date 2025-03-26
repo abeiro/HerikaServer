4c41de42108599e71f9cf9ca20b8e7c8a3faceec
daikichi <dekanaberserker@gmail.com>
2025-03-26 16:00:51 +0900

    log uncaught errors, allow no timestamp

--- lib/logger.php
+++ lib/logger.php
@@ -10,14 +10,24 @@ class Logger {
         'error' => 5,
     ];
 
-    private static $_minLogLevel = 'trace'; // Default minimum log level
+    // minimum log level to write to the log (default = write everything)
+    private static $_minLogLevel = 'trace';
 
+    // timestamp format (default = ISO 8601)
+    private static $_timestampFormat = 'Y-m-d\TH:i:sP';
+
+    // Ex: Logger::setLevel("warn") to suppress trace, debug, and info messages
     public static function setLevel($level) {
         if (isset(self::LOG_LEVELS[$level])) {
             self::$_minLogLevel = $level;
         } else {
             error_log("[error] Invalid log level specified: {$level}");
         }
+    }
+
+    // Can call with no parameter or an empty string to omit the timestamp from logs
+    public static function setTimestampFormat($format = "") {
+        self::$_timestampFormat = $format;
     }
 
     private static function shouldLog($level) {
@@ -29,8 +39,8 @@ class Logger {
             return;
         }
 
-        $timestamp = date('Y-m-d\TH:i:sP');
-        $logEntry = "[{$timestamp}] [{$level}] {$message}\n";
+        $timestamp = self::$_timestampFormat ? "[".date(self::$_timestampFormat)."] " : "";
+        $logEntry = "{$timestamp}[{$level}] {$message}\n";
         error_log($logEntry, 3, $logFile);
 
         // also write to apache error log
@@ -59,6 +69,45 @@ class Logger {
     public static function error($message, $logFile = self::DEFAULT_LOG) {
         Logger::log("error", $message, $logFile);
     }
+
+    // write uncaught errors to the CHIM log in addition to the apache log
+    public static function errorHandler(int $errno, string $errstr, string $errfile, int $errline): bool
+    {
+        switch ($errno) {
+            case E_USER_ERROR:
+            case E_ERROR:
+            case E_CORE_ERROR:
+            case E_COMPILE_ERROR:
+                $level = 'error';
+                break;
+            case E_WARNING:
+            case E_USER_WARNING:
+            case E_CORE_WARNING:
+            case E_COMPILE_WARNING:
+                $level = 'warn';
+                break;
+            case E_NOTICE:
+            case E_USER_NOTICE:
+                $level = 'info';
+                break;
+            default:
+                $level = 'debug'; // For other less critical errors
+                break;
+        }
+
+        $timestamp = self::$_timestampFormat ? "[".date(self::$_timestampFormat)."] " : "";
+        $logEntry = "{$timestamp}[{$level}] %s in %s on line %d\n";
+        $formattedMessage = sprintf($logEntry, $errstr, $errfile, $errline);
+
+        // write to the default log file (avoid error_log here because it would duplicate the message)
+        file_put_contents(self::DEFAULT_LOG, $formattedMessage, FILE_APPEND);
+
+        // return false to allow PHP's default error handler to run as well
+        return false;
+    }
 }
+
+// use the errorHandler provided here to write errors to the CHIM log in addition to the apache log
+set_error_handler(["Logger", "errorHandler"]);
 
 ?>
