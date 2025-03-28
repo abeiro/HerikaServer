528137113f8b4b1883d5944aaa1a7f948c06377b
daikichi <dekanaberserker@gmail.com>
2025-03-29 01:00:28 +0900

    fix permissions error

--- lib/model_dynmodel.php
+++ lib/model_dynmodel.php
@@ -51,8 +51,8 @@ function DMsetCurrentModel($model) {
         
     $file=__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."data".DIRECTORY_SEPARATOR."CurrentModel_{$lprof}.json";
 
+    shell_exec("chmod 775 $file");
     $cmj=file_put_contents($file,json_encode($model));
-	chmod($file, "775");
 
 }
 
