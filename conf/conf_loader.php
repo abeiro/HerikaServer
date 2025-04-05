42a631b309056f994ca7f97404b99a3156aa8398
vrpdont <vrpdont@gmail.com>
2025-04-04 22:49:36 -0400

    Update conf_loader to allow plugins to add their own settings
    This will scan every plugin in the `ext` folder in the server. Any plugin that wants to add their own schema to a conf, needs to have the following:
    
    * Add a `conf` folder to the plugin
    * Add a php file inside that `conf` directory named `<plugin name her>_conf.php`
    
    Inside that php file, you can add new properties that you want to the conf.
    
    An example would be:
    
    ```php
    
    <?php
    function get_myplugin_schema() {
        return [
            "MYPLUGIN" => [
                "_title" => "My Plugin Settings",
                "MAIN_SETTINGS" => [
                    "_title" => "Main settings",
                    "ENABLED" => [
                        "userlvl" => "basic",
                        "type" => "boolean",
                        "description" => "Enable this plugin."
                    ],
                    "ADVANCED_SETTING" => [
                        "userlvl" => "pro",
                        "type" => "string",
                        "description" => "An advanced setting for the plugin."
                    ],
                    "EXPERIMENTAL_SETTING" => [
                        "userlvl" => "wip",
                        "type" => "longstring",
                        "description" => "An experimental setting for the plugin"
                    ]
                ],
                "Secondary_Settings" => [
                    "_title" => "Optional Settings",
                    "Example_item" => [
                        "userlvl" => "basic",
                        "type" => "boolean",
                        "description" => "Example item in group"
                    ]
                ],
            ]
        ];
    }
    ?>
    ```

--- conf/conf_loader.php
+++ conf/conf_loader.php
@@ -6,6 +6,23 @@ function conf_loader_load() {
 
 	$confSchema=json_decode(file_get_contents($localPath."conf_schema.json"),true);
 
+	// Load plugin schemas from ext/*/conf/*_conf.php
+	$pluginSchemas = [];
+	$extDir = $rootPath . "ext" . DIRECTORY_SEPARATOR;
+	$pluginFolders = glob($extDir . "*", GLOB_ONLYDIR);
+	foreach ($pluginFolders as $folder) {
+		$pluginName = basename($folder);
+		$confDir = $folder . DIRECTORY_SEPARATOR . "conf" . DIRECTORY_SEPARATOR;
+		$pluginConfigs = glob($confDir . "*_conf.php");
+		foreach ($pluginConfigs as $pluginConfig) {
+			include_once $pluginConfig;
+			$schemaFunc = "get_{$pluginName}_schema";
+			if (function_exists($schemaFunc)) {
+				$pluginSchemas[$pluginName] = $schemaFunc();
+			}
+		}
+	}
+
 	$langFolders=scandir($rootPath."lang".DIRECTORY_SEPARATOR);
 	foreach ($langFolders as $n=>$folder)
 		if (!is_dir($rootPath."lang".DIRECTORY_SEPARATOR.$folder))
@@ -45,6 +62,30 @@ function conf_loader_load() {
 		}
 	}
 
+	// Merge plugin schemas into confMap, pulling values from $GLOBALS
+	foreach ($pluginSchemas as $pluginName => $pluginSchema) {
+		foreach ($pluginSchema as $name => $definition) {
+			if (isset($definition["type"])) {
+				$definition["currentValue"] = $GLOBALS[$name] ?? null;
+				$confMap[$name] = $definition;
+			} else if (is_array($definition)) {
+				foreach ($definition as $name2 => $definition2) {
+					if (isset($definition2["type"])) {
+						$definition2["currentValue"] = $GLOBALS[$name][$name2] ?? null;
+						$confMap["$name $name2"] = $definition2;
+					} else if (is_array($definition2)) {
+						foreach ($definition2 as $name3 => $definition3) {
+							if (isset($definition3["type"])) {
+								$definition3["currentValue"] = $GLOBALS[$name][$name2][$name3] ?? null;
+								$confMap["$name $name2 $name3"] = $definition3;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 	return $confMap;
 }
 
@@ -54,6 +95,41 @@ function conf_loader_load_titles() {
 	$rootPath=__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR;
 
 	$confSchema=json_decode(file_get_contents($localPath."conf_schema.json"),true);
+
+	// Load plugin titles from ext/*/conf/*_conf.php
+	$extDir = $rootPath . "ext" . DIRECTORY_SEPARATOR;
+	$pluginFolders = glob($extDir . "*", GLOB_ONLYDIR);
+	foreach ($pluginFolders as $folder) {
+		$pluginName = basename($folder);
+		$confDir = $folder . DIRECTORY_SEPARATOR . "conf" . DIRECTORY_SEPARATOR;
+		$pluginConfigs = glob($confDir . "*_conf.php");
+		foreach ($pluginConfigs as $pluginConfig) {
+			include_once $pluginConfig;
+			$schemaFunc = "get_{$pluginName}_schema";
+			if (function_exists($schemaFunc)) {
+				$pluginSchema = $schemaFunc();
+				foreach ($pluginSchema as $name => $definition) {
+					if (isset($definition["_title"])) {
+						$titleMap[$name] = $definition["_title"];
+					}
+					if (is_array($definition)) {
+						foreach ($definition as $name2 => $definition2) {
+							if (isset($definition2["_title"])) {
+								$titleMap["$name $name2"] = $definition2["_title"];
+							}
+							if (is_array($definition2)) {
+								foreach ($definition2 as $name3 => $definition3) {
+									if (isset($definition3["_title"])) {
+										$titleMap["$name $name2 $name3"] = $definition3["_title"];
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
 	foreach ($confSchema as $name=>$definition) {
 		if (isset($definition["_title"])) {
@@ -85,6 +161,39 @@ function conf_loader_load_schema() {
 	$rootPath=__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR;
 
 	$confSchema=json_decode(file_get_contents($localPath."conf_schema.json"),true);
+
+	// Load plugin schemas from ext/*/conf/*_conf.php
+	$extDir = $rootPath . "ext" . DIRECTORY_SEPARATOR;
+	$pluginFolders = glob($extDir . "*", GLOB_ONLYDIR);
+	foreach ($pluginFolders as $folder) {
+		$pluginName = basename($folder);
+		$confDir = $folder . DIRECTORY_SEPARATOR . "conf" . DIRECTORY_SEPARATOR;
+		$pluginConfigs = glob($confDir . "*_conf.php");
+		foreach ($pluginConfigs as $pluginConfig) {
+			include_once $pluginConfig;
+			$schemaFunc = "get_{$pluginName}_schema";
+			if (function_exists($schemaFunc)) {
+				$pluginSchema = $schemaFunc();
+				foreach ($pluginSchema as $name => $definition) {
+					if (isset($definition["type"])) {
+						$confMap[$name] = $definition;
+					} else if (is_array($definition)) {
+						foreach ($definition as $name2 => $definition2) {
+							if (isset($definition2["type"])) {
+								$confMap["$name $name2"] = $definition2;
+							} else if (is_array($definition2)) {
+								foreach ($definition2 as $name3 => $definition3) {
+									if (isset($definition3["type"])) {
+										$confMap["$name $name2 $name3"] = $definition3;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
 	foreach ($confSchema as $name=>$definition) {
 		if (isset($definition["type"])) {
