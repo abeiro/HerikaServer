67d65c1298e41566a13b80195ce3b14d50e9ccfc
id1001-gthb <id1001@gmail.com>
2025-07-17 09:11:40 +0300

    Update connector storage for default profile Replacing 'CurrentModel_.json' with 'CurrentModel_72dc4b1c501563d149fec99eb45b45f1.json'. 'CurrentModel_.json' does not always contain the connector for the 'default' profile, because 'active_profile' is calculated in various places as md5(npcName) without testing the case where 'npcName' is 'The Narrator'. The convention that the connector is in the file 'CurrentModel_72dc4b1c501563d149fec99eb45b45f1.json' corresponding to the case where 'active_profile' is md5('The narrator') was implemented in 'model_dynmodel.php'.

--- debug/simple_llm_request_with_context.php
+++ debug/simple_llm_request_with_context.php
@@ -13,7 +13,7 @@ $GLOBALS["SCRIPTLINE_ANIMATION"]="";
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$file = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."data".DIRECTORY_SEPARATOR.'CurrentModel_.json';
+$file = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."data".DIRECTORY_SEPARATOR.'CurrentModel_72dc4b1c501563d149fec99eb45b45f1.json';
 $enginePath = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR;
 
 
