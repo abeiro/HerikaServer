ab103495a659ffde69bcd21d6c624f5becd80848
id1001-gthb <id1001@gmail.com>
2025-09-10 11:14:11 +0300

    TIME_AWARENESS plugin rework - reworked the logic by which previous interactions are retrieved. - reworked time-related calculations/conversions. - reworked foreign language resources - depends on some new functions in data_functions.php

---
+++ ext/time_awareness/lang/es/ta_translation.php
@@ -0,0 +1,13 @@
+<?php
+
+//spanish:
+$sl_never_met       = "{$npc} y {$player} nunca se han conocido.";
+$sl_first_time      = "{$npc} habla con {$player} por primera vez.";
+$sl_first_time_1h   = "{$npc} y {$player} hablaron por primera vez hace menos de una hora.";
+$sl_hours_ago       = "{$npc} y {$player} hablaron hace {$inGameHours} horas.";
+$sl_days_ago        = "{$npc} y {$player} hablaron hace {$inGameDays} días.";
+$sl_weeks_ago       = "{$npc} y {$player} hablaron hace unas semanas.";
+$sl_months_ago      = "{$npc} y {$player} hablaron hace unos meses.";
+$sl_years_ago       = "{$npc} y {$player} hablaron hace años.";
+
+?>
---
+++ ext/time_awareness/lang/pl/ta_translation.php
@@ -0,0 +1,13 @@
+<?php
+
+//polish:
+$sl_never_met 		= "{$npc} i {$player} nigdy się nie spotkali."; 
+$sl_first_time 		= "{$npc} rozmawia po raz pierwszy z {$player}.";
+$sl_first_time_1h 	= "{$npc} i {$player} rozmawiali po raz pierwszy mniej niż godzinę temu."; 
+$sl_hours_ago 		= "{$npc} i {$player} rozmawiali {$inGameHours} godzin temu."; 
+$sl_days_ago 		= "{$npc} i {$player} rozmawiali {$inGameDays} dni temu."; 
+$sl_weeks_ago 		= "{$npc} i {$player} rozmawiali kilka tygodni temu.";
+$sl_months_ago 		= "{$npc} i {$player} rozmawiali kilka miesięcy temu."; 
+$sl_years_ago 		= "{$npc} i {$player} rozmawiali lata temu.";
+
+?>
