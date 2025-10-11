1defbf8e9b6a6a1a2c69464f86b33489a7372b79
abeiro <augusto.beiro@gmail.com>
2025-10-11 23:15:17 +0200

    Update vector-compact-chromadb.php

--- ui/tests/vector-compact-chromadb.php
+++ ui/tests/vector-compact-chromadb.php
@@ -54,7 +54,7 @@ error_reporting(E_ALL);
 $embedding = $FEATURES["MEMORY_EMBEDDING"]["TEXT2VEC_PROVIDER"];
 
 //Run the Compact Command
-$commandcompact = 'php /var/www/html/HerikaServer/debug/util_memory_subsystem.php compact noembed';
+$commandcompact = 'php /var/www/html/HerikaServer/debug/util_memory_subsystem.php compact';
 $commandcompact = shell_exec($commandcompact);
 echo '<link rel="stylesheet" type="text/css" href="../css/main.css">';
 echo "<title> CHIM - Compact Memories</title>";
@@ -65,7 +65,7 @@ echo "<h1>Compact Memories</h1>";
 echo "<pre class='command-output'>$commandcompact</pre>";
 
 //Run the Sync Command
-$commandsync = 'php /var/www/html/HerikaServer/debug/util_memory_subsystem.php sync';
+$commandsync = 'php /var/www/html/HerikaServer/debug/util_memory_subsystem.php resync';
 $outputsync = shell_exec($commandsync);
     echo "<br>";
     echo "<h1>Memory Sync for TXT2VEC</h1>";
