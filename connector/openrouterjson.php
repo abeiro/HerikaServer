7490cc7e11d4094cf576c7aae717c4e77fb40d25
Augusto Beiro <augusto.beiro@gmail.com>
2025-11-09 09:35:03 +0100

    * Added case for grok (reasining must be enabled)

--- connector/openrouterjson.php
+++ connector/openrouterjson.php
@@ -599,6 +599,9 @@ class openrouterjson
             if (!(stripos($this->_model, "qwen3-") === false)) {//qwen3
                 $data["enable_thinking"] = false;
             }            
+            if (stripos($this->_model, "grok-3-mini") != false) {//grok-3-mini needs reasoning cand cannot be disabled
+                $data["reasoning"]["enabled"] = true;
+            }            
         }
         
         if ($this->_is_openai) {
@@ -1201,8 +1204,11 @@ class openrouterjson
             //Logger::debug("reasoning " . $this->_model);
             if (!(stripos($this->_model, "qwen3-") === false)) {//qwen3
                 $data["enable_thinking"] = false;
-            }            
-        }
+            }       
+            if (stripos($this->_model, "grok-3-mini") != false) {//grok-3-mini needs reasoning cand cannot be disabled
+                $data["reasoning"]["enabled"] = true;
+            }        
+    }
         
         if ($this->_is_openai) {
             // OpenAI models use max_completion_tokens
