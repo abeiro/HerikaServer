23d8a535da1c3a810090f02d64be774bfa227cab
kakoi-iii <255952346+kakoi-iii@users.noreply.github.com>
2026-02-04 14:07:05 -0500

    chore: revert superfluous change.

--- connector/groqjson.php
+++ connector/groqjson.php
@@ -354,15 +354,13 @@ class groqjson
             unset($data["max_tokens"]); 
         }
 
-
-        // Merge extra_parameters LAST, so they always overwrite previous keys
         if (isset($GLOBALS["CONNECTOR"][$this->name]["extra_parameters"]) && is_array($GLOBALS["CONNECTOR"][$this->name]["extra_parameters"])) {
             foreach ($GLOBALS["CONNECTOR"][$this->name]["extra_parameters"] as $k=>$v) {
                 $data[$k]=$v;
             }
         }
 
-        $GLOBALS["DEBUG_DATA"]["full"]=(($data));
+        $GLOBALS["DEBUG_DATA"]["full"]=($data);
 
         file_put_contents(__DIR__."/../log/context_sent_to_llm.log",date(DATE_ATOM)."\n=\n".var_export($data,true)."\n=\n", FILE_APPEND);
 
@@ -731,14 +729,13 @@ class groqjson
             unset($data["max_tokens"]); 
         }
 
-        // Merge extra_parameters LAST, so they always overwrite previous keys
         if (isset($GLOBALS["CONNECTOR"][$this->name]["extra_parameters"]) && is_array($GLOBALS["CONNECTOR"][$this->name]["extra_parameters"])) {
             foreach ($GLOBALS["CONNECTOR"][$this->name]["extra_parameters"] as $k=>$v) {
                 $data[$k]=$v;
             }
         }
 
-        $GLOBALS["DEBUG_DATA"]["full"]=(($data));
+        $GLOBALS["DEBUG_DATA"]["full"]=($data);
      
         $headers = array(
             'Content-Type: application/json',
