686e3321b035c60a0fba8505a6f89eab72cd09ef
id1001-gthb <id1001@gmail.com>
2026-02-09 10:51:19 +0200

    updates in logger.php and main.php - logger.php, adds the option to display the call stack when displaying errors starting with a preset level. The option is disabled by default. It can be adjusted according to personal preferences for example in a separate globals.php file. - added context_pre.php hook for context overrides on 'ext' dir plugins, called before system prompt build.

--- main.php
+++ main.php
@@ -2067,8 +2067,8 @@ if (isset($GLOBALS["TTSFUNCTION"]) && !empty($GLOBALS["TTSFUNCTION"])) {
 }
 
 
-// Check for context overrides on ext dir (plugins)
-requireFilesRecursively(__DIR__.DIRECTORY_SEPARATOR."ext".DIRECTORY_SEPARATOR,"context.php");
+// Check for context overrides on ext dir (plugins) before system prompt build
+requireFilesRecursively(__DIR__.DIRECTORY_SEPARATOR."ext".DIRECTORY_SEPARATOR,"context_pre.php");
 
 
 if (!empty($GLOBALS["OGHMA_HINT"])) {
@@ -2089,6 +2089,8 @@ if (!empty($GLOBALS["OGHMA_HINT"])) {
     $GLOBALS["COMMAND_PROMPT"] = "";
 }
 
+// Check for context overrides on ext dir (plugins) after system prompt build
+requireFilesRecursively(__DIR__.DIRECTORY_SEPARATOR."ext".DIRECTORY_SEPARATOR,"context.php");
 
 // audit_log(__FILE__." [PLUGINS CONTEXT]  ".__LINE__);
 
