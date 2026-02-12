d7a0b1e07fc3d20eb0ba311e7b0bee494478734f
Matt <easykill2k@gmail.com>
2026-02-12 19:09:36 +0000

    Remove unnecessary `substr` usage for title processing in book and comm modules

--- book.php
+++ book.php
@@ -31,7 +31,7 @@ if (!$_FILES["file"]["tmp_name"]) {
 $db = new sql();
 
 
-$title = substr($_GET["title"], 1);   // Initial strange "p" at the beginning.
+$title = $_GET["title"];   // Initial strange "p" at the beginning.
 $titleEsc = $db->escape($title);
 $alreadyinDb = $db->fetchOne("select * from books where title = '$titleEsc' and sess='generated' ");
 
