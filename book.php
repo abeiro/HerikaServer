c7658ea31be08f2e5b2f66f290c7b9d0afc05e2d
Matt <easykill2k@gmail.com>
2026-02-16 19:08:35 +0000

    Remove redundant comment in book title retrieval logic

--- book.php
+++ book.php
@@ -31,7 +31,7 @@ if (!$_FILES["file"]["tmp_name"]) {
 $db = new sql();
 
 
-$title = $_GET["title"];   // Initial strange "p" at the beginning.
+$title = $_GET["title"];
 $titleEsc = $db->escape($title);
 $alreadyinDb = $db->fetchOne("select * from books where title = '$titleEsc' and sess='generated' ");
 
