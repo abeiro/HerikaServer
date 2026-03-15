1ae53f853347aa588cff9e4a672754b5e1c2ffbb
RANGROO <reecemeakings@gmail.com>
2026-03-15 12:06:42 -0700

    bootstrap conf

--- debug/apply_db_updates.php
+++ debug/apply_db_updates.php
@@ -2,18 +2,36 @@
 // This file is for apply db updates via command line.
 
 error_reporting(E_ERROR);
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 $configFilepath = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."conf".DIRECTORY_SEPARATOR;
 $rootEnginePath = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR;
 
+$defaultConf = $rootEnginePath . "conf" . DIRECTORY_SEPARATOR . "conf.php";
+$sampleConf = $rootEnginePath . "conf" . DIRECTORY_SEPARATOR . "conf.sample.php";
+$confLoader = $rootEnginePath . "conf" . DIRECTORY_SEPARATOR . "conf_loader.php";
 
-require_once($rootEnginePath . "conf".DIRECTORY_SEPARATOR."conf.php");
+if (file_exists($sampleConf)) {
+    require_once($sampleConf);
+}
+if (file_exists($defaultConf)) {
+    require_once($defaultConf);
+}
+if (file_exists($confLoader)) {
+    require_once($confLoader);
+}
+
+if (empty($GLOBALS["DBDRIVER"])) {
+    $GLOBALS["DBDRIVER"] = "postgresql";
+}
 
 $configFilepath=realpath($configFilepath).DIRECTORY_SEPARATOR;
 
 // Profile selection
-$GLOBALS["PROFILES"]["default"]="$configFilepath/conf.php";
+$GLOBALS["PROFILES"] = is_array($GLOBALS["PROFILES"] ?? null) ? $GLOBALS["PROFILES"] : [];
+$GLOBALS["PROFILES"]["default"] = file_exists($defaultConf) ? $defaultConf : $sampleConf;
 foreach (glob($configFilepath . 'conf_????????????????????????????????????????????????.php') as $mconf ) {
     if (file_exists($mconf)) {
         $filename=basename($mconf);
@@ -28,12 +46,12 @@ if (isset($_SESSION["PROFILE"]) && in_array($_SESSION["PROFILE"],$GLOBALS["PROFI
     if (file_exists($_SESSION["PROFILE"]))
         require_once($_SESSION["PROFILE"]);
     else {
-        $_SESSION["PROFILE"]="$configFilepath/conf.php";
+        $_SESSION["PROFILE"] = $GLOBALS["PROFILES"]["default"];
         
     }
 
 } else
-    $_SESSION["PROFILE"]="$configFilepath/conf.php";
+    $_SESSION["PROFILE"] = $GLOBALS["PROFILES"]["default"];
 // End of profile selection
 
 
