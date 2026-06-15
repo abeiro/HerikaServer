5468ad59b1659551ba14ff108dae80d0cf3fbfba
Augusto Beiro <augusto.beiro@gmail.com>
2026-06-15 09:26:26 +0200

    fix: chat logging for suggestion requests to use people in range

--- lib/chat_helper_functions.php
+++ lib/chat_helper_functions.php
@@ -1669,7 +1669,21 @@ function returnLines($lines,$writeOutput=true)
                 )
             );
             
-            // RECHAT
+            // CHAT LOGGING
+
+            // Patch. New 'people in range' code is not working properly when request is a suggestion.
+            // Check line 1665 on main.php, probable some needed codepath is not being executed when request is a suggestion 
+            // Probably other requests generating speech (not user inputs) are also affected.
+
+            if ($GLOBALS["gameRequest"][0]=="suggestion") {
+                
+                $forcedPeople=DataBeingsInCloseRange(true);
+                error_log("[WARNING] Suggestion detected, forcing people in range to be logged: $forcedPeople");
+            } else {
+                // error_log("[BUG] Suggestion not detected, {$GLOBALS["gameRequest"][0]}");
+                $forcedPeople='';
+            }
+
             $originalRequest=$GLOBALS["gameRequest"];
             $originalRequest[0]="prechat";
             $originalRequest[1]++;
@@ -1690,7 +1704,7 @@ function returnLines($lines,$writeOutput=true)
                 $addonlistener="";
             }
             $originalRequest[3]="{$outBuffer["actor"]}: $responseForContext $addonlistener";
-            logEvent($originalRequest);
+            logEvent($originalRequest,$forcedPeople);
             
             // Log chat here, because  function return comes back out of sync.
             $originalRequest[0]="chat";
@@ -1715,7 +1729,7 @@ function returnLines($lines,$writeOutput=true)
                 'utterance_id' => $GLOBALS["SCRIPTLINE_UTTERANCE_ID"] ?? chimGenerateUtteranceId(),
                 'delivery_state' => 'emitted'
             ];
-            logEvent($originalRequest);
+            logEvent($originalRequest,$forcedPeople);
         }
         
     }
