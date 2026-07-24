432fd69d0bec0e251efb2840499be159e3dab07c
RANGROO <reecemeakings@gmail.com>
2026-07-23 20:08:18 -0700

    Refresh outdated snapshot clone functions

--- unittests/tests/SchemaCloneIdentityTest.php
+++ unittests/tests/SchemaCloneIdentityTest.php
@@ -2,8 +2,27 @@
 
 use PHPUnit\Framework\TestCase;
 
+require_once(
+    __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..'
+    . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'playthrough_schema.php'
+);
+
 final class SchemaCloneIdentityTest extends TestCase
 {
+    public function testLegacyCloneFunctionNeedsRefresh(): void
+    {
+        $legacyDefinition = 'INSERT INTO destination SELECT * FROM source';
+
+        $this->assertFalse(pts_clone_function_is_current($legacyDefinition));
+    }
+
+    public function testIdentityAwareCloneFunctionIsCurrent(): void
+    {
+        $currentDefinition = 'INSERT INTO destination OVERRIDING SYSTEM VALUE SELECT * FROM source';
+
+        $this->assertTrue(pts_clone_function_is_current($currentDefinition));
+    }
+
     public function testSchemaClonePreservesGeneratedIdentityValues(): void
     {
         $sql = file_get_contents(
