5e2fe3a2c0736eb3949fd1f90807cd77c30aa440
RANGROO <reecemeakings@gmail.com>
2026-07-29 09:20:10 -0700

    Remove false southern map basin

--- ui/mapview.php
+++ ui/mapview.php
@@ -574,7 +574,7 @@ if (!function_exists('race_icon_web_path')) {
     $mapHeight = 1625;
 
     // Map file path (relative to web root)
-$mapImageUrl = '../data/maps/Map_of_Skyrim_dark.png?v=2';
+$mapImageUrl = '../data/maps/Map_of_Skyrim_dark.png?v=3';
 
     // Function to translate in-game coordinates to map coordinates
     function translateCoords($ingameX, $ingameY, $mapWidth, $mapHeight, $worldXMin, $worldXMax, $worldYMin, $worldYMax)
