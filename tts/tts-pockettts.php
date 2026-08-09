83b6e1a056eebf25ddb1565fd3578270b4e21018
Augusto Beiro <augusto.beiro@gmail.com>
2026-08-09 20:01:19 +0200

    Enhance audio processing in PocketTTS by integrating audiofilterd and adding fallback to ffmpeg for error handling

--- tts/tts-pockettts.php
+++ tts/tts-pockettts.php
@@ -324,11 +324,13 @@ function num2kan_decimal($instr) {
 	return $outstr;
 }
 
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "audiofilterd_client.php";
 
 $GLOBALS["TTS_IN_USE"]=function($textString, $mood , $stringforhash) {
 
 		//pockettts_settings([]); //Check this
 		
+
 		if (isset($GLOBALS["AVOID_TTS_CACHE"]) && $GLOBALS["AVOID_TTS_CACHE"]===false )
 			if (file_exists(dirname((__FILE__)) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "soundcache/" . md5(trim($stringforhash)) . ".wav"))
 				return dirname((__FILE__)) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "soundcache/" . md5(trim($stringforhash)) . ".wav";
@@ -465,7 +467,23 @@ $GLOBALS["TTS_IN_USE"]=function($textString, $mood , $stringforhash) {
 			file_put_contents($oname, $response); // Save the audio response to a file
 			$startTimeTrans = microtime(true);
 			//shell_exec("ffmpeg -y -i $oname  -af \"adelay=150|150,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-25dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-40dB,areverse,speechnorm=e=3:r=0.0001:l=1:p=0.75\" $fname 2>/dev/null >/dev/null");
-			shell_exec("ffmpeg -y -i ".escapeshellarg($oname)."  $FFMPEG_FILTER ".escapeshellarg($fname)." 2>/dev/null >/dev/null");
+			//shell_exec("ffmpeg -y -i ".escapeshellarg($oname)."  $FFMPEG_FILTER ".escapeshellarg($fname)." 2>/dev/null >/dev/null");
+			if (!processAudio(
+					$sourceBinaryData = $response,
+					$filters = [
+						[
+							'type' => 'trim_start',
+							'milliseconds' => 250.0,
+						]
+					],
+					$outputFilenameWithPath = $fname,
+					'/var/www/html/HerikaServer/tts/audiofilterd.sock'
+			)) {
+				//Fallback to ffmpeg if audiofilterd fails
+				Logger::error("Audio processing failed for PocketTTS response. ".__FILE__." ".__LINE__." ".__FUNCTION__);
+				shell_exec("ffmpeg -y -i ".escapeshellarg($oname)."  $FFMPEG_FILTER ".escapeshellarg($fname)." 2>/dev/null >/dev/null");
+				
+			};
 			//error_log("ffmpeg -y -i $oname  $FFMPEG_FILTER $fname ".__FILE__." ".__LINE__." ".__FUNCTION__);
 			$endTimeTrans = microtime(true)-$startTimeTrans;
 			
